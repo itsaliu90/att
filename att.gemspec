29b4f48b7fdceeea7247ef1f99f7590827109915
Alex Liu <yuningalexliu@gmail.com>
2016-09-09 00:20:08 -0400

    mvp

--- att.gemspec
+++ att.gemspec
@@ -2,6 +2,9 @@ Gem::Specification.new do |s|
   s.name        = 'att'
   s.version     = '0.0.0'
   s.date        = '2016-09-08'
+  s.executables << 'att'
+  s.add_runtime_dependency "chronic",
+      ["= 0.10.2"]
   s.summary     = "att"
   s.description = "run terminal commands 'att' a specific time"
   s.authors     = ["Alex Liu"]
--- lib/att.rb
+++ lib/att.rb
@@ -1,5 +1,20 @@
+require 'chronic'
+
 class Att
-    def self.hi
-        puts "Hi, I'm Att"
+    def self.new_task(scheduled_time, command)
+    	frozen_scheduled_time = Chronic.parse(scheduled_time)
+    	while true do
+    		puts "scheduled time is #{frozen_scheduled_time}"
+    		puts "current time is #{Time.now}"
+    #		binding.pry
+    		if Time.now >= frozen_scheduled_time
+    			puts "about to execute command '#{command}'"
+        		output = `#{command}`
+        		STDOUT.puts output
+        		break
+        	end
+        	sleep(5)
+        	STDOUT.puts "sleeping for 5 seconds..."
+    	end
     end
 end
